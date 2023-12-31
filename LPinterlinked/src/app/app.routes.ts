e388c7834bd0ecacb6a65ae16996cdf4662746de
David Iacono <davidiacono@Davids-MacBook-Pro.local>
2023-12-31 16:11:23 +1100

    provided HttpClientModule

--- LPinterlinked/src/app/app.routes.ts
+++ LPinterlinked/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PredatorCapComponent } from './shared/components/predator-cap/predator-cap.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: PredatorCapComponent,
+        path: " "
+    }
+];
--- LPinterlinked/src/main.ts
+++ LPinterlinked/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { PredatorCapComponent } from './app/shared/components/predator-cap/predator-cap.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(PredatorCapComponent, appConfig)
   .catch((err) => console.error(err));
