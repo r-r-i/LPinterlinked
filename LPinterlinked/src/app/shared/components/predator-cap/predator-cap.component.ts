a941d6ee9c1331bd2a4205bb814e87a3b9d639bf
David Iacono <davidiacono@Davids-MacBook-Pro.local>
2023-12-31 15:25:56 +1100

    set up predator cap component

--- LPinterlinked/src/app/shared/components/predator-cap/predator-cap.component.ts
+++ LPinterlinked/src/app/shared/components/predator-cap/predator-cap.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApexDataService } from '../../services/apex-data.service';
+import { IPredatorCaps } from '../../types/predator-cap.model';
 
 @Component({
   selector: 'app-predator-cap',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './predator-cap.component.html',
   styleUrl: './predator-cap.component.css'
 })
-export class PredatorCapComponent {
+export class PredatorCapComponent { 
+  predatorData: IPredatorCaps;
 
+  constructor(private apexDataService: ApexDataService) {}
+
+  ngOnInit(): void {
+    this.initPredatorCap();
+  }
+
+  private initPredatorCap() {
+    this.apexDataService
+    .getPredatorCap()
+    .subscribe((predatorCap: IPredatorCaps) => {
+      this.predatorData = predatorCap;
+      console.log(this.predatorData);
+    });
+  }
 }
